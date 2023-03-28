bd1d135d88b0d6b31a57f18168b319b926dbfe90
LtFubuki <99317076+LtFubuki@users.noreply.github.com>
2023-03-27 18:59:32 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,14 @@
 DOMAIN="alicam.org"
 EMAIL="youremail@example.com"
 
-# Create the directory for SSL certificates
-mkdir -p /etc/nginx/certs
+# Create the directory for SSL certificates and webroot
+mkdir -p /etc/nginx/certs /var/www/html
+
+# Register the account
+certbot register --non-interactive --agree-tos --email "$EMAIL" || true
 
 # Obtain the SSL certificate
-certbot certonly --standalone --non-interactive --agree-tos --email "$EMAIL" -d "$DOMAIN" || true
+certbot certonly --webroot --webroot-path /var/www/html --non-interactive --agree-tos --email "$EMAIL" -d "$DOMAIN" || true
 
 # Create symbolic links for the SSL certificates
 ln -sf /etc/letsencrypt/live/"$DOMAIN"/fullchain.pem /etc/nginx/certs/alicam.org.crt
