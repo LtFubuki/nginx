81630fee157cebfb1841e9a1c78fbdaaa55a9d5e
LtFubuki <99317076+LtFubuki@users.noreply.github.com>
2023-03-27 19:01:53 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,10 +7,10 @@ EMAIL="youremail@example.com"
 mkdir -p /etc/nginx/certs /var/www/html
 
 # Register the account
-certbot register --non-interactive --agree-tos --email "$EMAIL" || true
+certbot register --non-interactive --agree-tos --email "$EMAIL" --server "https://acme-v02.api.letsencrypt.org/directory" || true
 
 # Obtain the SSL certificate
-certbot certonly --webroot --webroot-path /var/www/html --non-interactive --agree-tos --email "$EMAIL" -d "$DOMAIN" || true
+certbot certonly --webroot --webroot-path /var/www/html --non-interactive --agree-tos --email "$EMAIL" -d "$DOMAIN" --server "https://acme-v02.api.letsencrypt.org/directory" || true
 
 # Create symbolic links for the SSL certificates
 ln -sf /etc/letsencrypt/live/"$DOMAIN"/fullchain.pem /etc/nginx/certs/alicam.org.crt
