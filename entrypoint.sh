7b35aab6b757671e01e467a68584e486f64804d2
LtFubuki <99317076+LtFubuki@users.noreply.github.com>
2023-03-27 19:05:58 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,15 +6,15 @@ EMAIL="youremail@example.com"
 # Create the directory for SSL certificates and webroot
 mkdir -p /etc/nginx/certs /var/www/html
 
-# Register the account
-certbot register --non-interactive --agree-tos --email "$EMAIL" --server "https://acme-v02.api.letsencrypt.org/directory" || true
+# Export the path to acme.sh
+export PATH="$HOME/.acme.sh:$PATH"
 
 # Obtain the SSL certificate
-certbot certonly --webroot --webroot-path /var/www/html --non-interactive --agree-tos --email "$EMAIL" -d "$DOMAIN" --server "https://acme-v02.api.letsencrypt.org/directory" || true
+acme.sh --issue --webroot /var/www/html --domain "$DOMAIN" --email "$EMAIL" --reloadcmd "nginx -s reload" || true
 
 # Create symbolic links for the SSL certificates
-ln -sf /etc/letsencrypt/live/"$DOMAIN"/fullchain.pem /etc/nginx/certs/alicam.org.crt
-ln -sf /etc/letsencrypt/live/"$DOMAIN"/privkey.pem /etc/nginx/certs/alicam.org.key
+ln -sf "$HOME/.acme.sh/$DOMAIN/fullchain.cer" /etc/nginx/certs/alicam.org.crt
+ln -sf "$HOME/.acme.sh/$DOMAIN/$DOMAIN.key" /etc/nginx/certs/alicam.org.key
 
 # Start Nginx in the foreground
 nginx -g "daemon off;"
